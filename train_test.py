2bb5a93ff4c4c3ac5515eb9389ffa682c2b30ee6
kishimoto-banana <drehbleistift@gmail.com>
2019-01-05 00:42:56 +0900

    Add logistic regression exec by dataproc

--- train_test.py
+++ train_test.py
@@ -1,7 +1,7 @@
 import sys
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import StringIndexer
-from pyspark.ml.feature import OneHotEncoder
+from pyspark.ml.feature import OneHotEncoderEstimator
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.feature import PCA
 from pyspark.ml.classification import LogisticRegression
@@ -17,7 +17,6 @@ input_file_path = sys.argv[1]
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('criteo').config('spark.some.config.option', 'some-value').getOrCreate()
 df = spark.read.csv(input_file_path, encoding='utf-8', inferSchema=True, sep='\t')
-df.show()
 
 # 特徴量と正解ラベルの定義
 label_idx = 0
@@ -28,45 +27,25 @@ category_features = [f'_c{i}' for i in range(14, 40)]
 df = df.fillna({feature:-1 for feature in real_features})
 df = df.fillna({feature:'NULL' for feature in category_features})
 
-# 文字列のラベル化
-# indexers = [StringIndexer(inputCol=feature, outputCol=f'{feature}_idx').fit(df) for feature in category_features]
-# pipeline = Pipeline(stages=indexers)
-# indexed = pipeline.fit(df).transform(df)
+# 学習・テストデータに分割
+train, test = df.randomSplit([0.8, 0.2], seed=42)
 
-indexed = df
-for feature in category_features:
-    indexer = StringIndexer(inputCol=feature, outputCol=f'{feature}_idx')
-    model = indexer.fit(indexed)
-    indexed = model.transform(indexed)
-
-# one-hot encoding
-indexed_features = [f'{feature}_idx' for feature in category_features]
-onehot_features = [f'{feature}_vec' for feature in category_features]
-encoded = indexed
-for indexed_feature, onehot_feature in zip(indexed_features, onehot_features):
-    encoder = OneHotEncoder(inputCol=indexed_feature, outputCol=onehot_feature, dropLast=False)
-    encoded = encoder.transform(encoded)
-
-# モデルの入力データへの変換
-assembler = VectorAssembler(inputCols=real_features+onehot_features, outputCol='assembles')
-assembled = assembler.transform(encoded)
-
-# PCA
-pca = PCA(k=2, inputCol='assembles', outputCol='features')
-model = pca.fit(assembled)
-X = model.transform(assembled)
-
-# 学習とテストに分割
-train, test = X.randomSplit([0.8, 0.2], seed=42)
-train.show()
-
-# ロジスティック回帰の学習
+# パイプラインの構築
+indexers = [StringIndexer(inputCol=feature, outputCol=f'{feature}_idx',  handleInvalid='keep') for feature in category_features]
+encoder = OneHotEncoderEstimator(inputCols=[f'{feature}_idx' for feature in category_features], outputCols=[f'{feature}_vec' for feature in category_features], dropLast=False, handleInvalid='keep')
+assembler = VectorAssembler(inputCols=real_features+[f'{feature}_vec' for feature in category_features], outputCol='assembles')
+pca = PCA(k=100, inputCol='assembles', outputCol='features')
 lr = LogisticRegression(featuresCol='features', labelCol=f'_c{label_idx}', maxIter=100)
-model = lr.fit(train)
-print('Coefficients: ' + str(model.coefficients))
-print('Intercept: ' + str(model.intercept))
+stages = indexers
+stages.append(encoder)
+stages.append(assembler)
+stages.append(pca)
+stages.append(lr)
+pipeline = Pipeline(stages=stages)
 
-# 予測
+model = pipeline.fit(train)
+print(model.stages[-1].coefficients)
+
 predictions = model.transform(test)
 evaluator = MulticlassClassificationEvaluator(labelCol=f'_c{label_idx}', metricName='f1')
 f1 = evaluator.evaluate(predictions)
